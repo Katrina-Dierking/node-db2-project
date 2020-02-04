b48fc0f3c15024e9d218cbdc0fceb6d459ccfed2
Katrina_Dierking <kalynndier@gmail.com>
2020-02-04 15:44:36 -0500

    routes/knex/etc created-- migrating

---
+++ data/dbConfig.js
@@ -0,0 +1,8 @@
+const knex = require('knex');
+
+const configOptions = require('../knexfile');
+
+
+const db = knex(configOptions.development);
+
+module.exports = db;
---
+++ router/carsRouter.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const db = require('../data/dbConfig');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const cars = await db('cars');
+        res.json(cars);
+    } catch(err){
+        res.status(500).json
+        ({
+            success: false, 
+            errorMessage: 'cannot get cars'
+        })
+    }
+})
+
+router.post('/', async (req, res) => {
+    const postBody = req.body;
+    try {
+        const cars = await db('cars').insert(postBody);
+        res.status(201).json(cars);
+    } catch(err){
+        res.status(500).json
+        ({
+            success: false,
+            errorMessage: 'car not posted'
+        })
+    }
+})
+
+
+
+module.exports = router;
